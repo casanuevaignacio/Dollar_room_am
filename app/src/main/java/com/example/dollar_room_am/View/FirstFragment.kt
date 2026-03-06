52f5ff6c944eb74295c3f291a52d7dd76b54eb28
Ignacio Casanueva <casanueva.ignacio@gmail.com>
2026-03-06 11:49:11 -0300

    segunda parte

--- app/src/main/java/com/example/dollar_room_am/View/FirstFragment.kt
+++ app/src/main/java/com/example/dollar_room_am/View/FirstFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.room.Room
+import com.example.dollar_room_am.Modelo.TransactionRepository
 import com.example.dollar_room_am.R
+import com.example.dollar_room_am.ViewModel.TransactionViewModel
+import com.example.dollar_room_am.ViewModel.TransactionViewModelFactory
 import com.example.dollar_room_am.databinding.FragmentFirstBinding
 
 /**
@@ -19,6 +25,15 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    // Adapter para RecyclerView
+    private val adapter= TransactionAdapter()
+    // Repository y ViewModel
+    private lateinit var repository: TransactionRepository
+
+    private val viewModel : TransactionViewModel by viewModels {
+
+        TransactionViewModelFactory(repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,10 +48,33 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
     }
+
+
+    private fun handleTransaction(type :String){
+
+
+        val amountText= binding.amountEditText.text.toString()
+        if (amountText.isEmpty()) {
+            Toast.makeText(requireContext(), "Ingrese un monto", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        val amount = amountText.toDouble()
+        // guadando en la bd
+        viewModel.addTransaction(type,amount)  //agrega al repositorio y actualiza LiveData
+        binding.amountEditText.text.clear()
+        Toast.makeText(requireContext(), "$type $amount USD registrado", Toast.LENGTH_SHORT).show()
+    }
+
+
+
+
+
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/dollar_room_am/View/MainActivity.kt
+++ app/src/main/java/com/example/dollar_room_am/View/MainActivity.kt
@@ -14,7 +14,7 @@ import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,38 +23,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        val firstFragment = FirstFragment()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        if (savedInstanceState == null) {
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+            //agregamos el fragment al layout
+            supportFragmentManager.beginTransaction()
+                .add(R.id.nav_host_fragment_content_main, firstFragment)
+                .commitNow()
+
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+
+
+
     }
 }
+
+
+
+
+
+
+
+
